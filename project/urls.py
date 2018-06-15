4872623bd5bc2ff6125fd47f33307675f93abfb0
Benjamin Merot <bem.its@dsg.dk>
2018-06-15 10:07:13 +0200

    Added URL path to load the Rest API GUI in path /api

--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path ('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
