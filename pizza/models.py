e9ca831121dabb2b49452815ce03ee365a9ac0ee
Benjamin Merot <bem.its@dsg.dk>
2018-06-15 12:37:04 +0200

    Added transactional part of project

--- pizza/models.py
+++ pizza/models.py
@@ -1,19 +1,38 @@
 from django.db import models
 
 
-class Food(models.Model):
+class Customer(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+
+
+class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
 
     price = models.DecimalField(..., max_digits=5, decimal_places=2)
+    vat = models.DecimalField(..., max_digits=5, decimal_places=2)
     currency = models.CharField(max_length=2)
+
+    class Meta:
+        abstract = True
+
+
+class Order(models.Model):
+    owner = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    placed_on = models.DateTimeField(auto_now_add=True)
+    shipped_on = models.DateTimeField(auto_now_add=True)
+
+    items = models.ManyToManyField(Product)
+
+
+class Food(Product):
 
     organic = models.BooleanField(default=False)
     vegetarian = models.BooleanField(default=False)
     vegan = models.BooleanField(default=False)
-
-    class Meta:
-        abstract = True
 
 
 class Burger(Food):
@@ -25,6 +44,7 @@ class Burger(Food):
 
 class Meal(Food):
     pass
+
 
 class Topping(Food):
     pass
@@ -40,5 +60,13 @@ class Pizza(Food):
 class Drink(Food):
     size = models.DecimalField(..., max_digits=3, decimal_places=2)
     alcohol = models.BooleanField(default=False)
+
+
+class Menu(Product):
+    # inherited price is expected to be cheaper than sum of items in menu
+    burgers = models.ManyToManyField(Burger)
+    drinks = models.ManyToManyField(Drink)
+    meals = models.ManyToManyField(Meal)
+    pizzas = models.ManyToManyField(Pizza)
 
 
--- pizza/serializers.py
+++ pizza/serializers.py
@@ -5,22 +5,28 @@ from . import models
 class BurgerSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Burger
-        fields = ('id', 'name', 'description',)
+        fields = ('id', 'name', 'description', 'price',)
 
 
 class DrinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Drink
-        fields = ('id', 'name', 'description',)
+        fields = ('id', 'name', 'description', 'price',)
 
 
 class MealSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Meal
-        fields = ('id', 'name', 'description',)
+        fields = ('id', 'name', 'description', 'price',)
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Meal
+        fields = ('id', 'name', 'description', 'price',)
 
 
 class PizzaSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Pizza
-        fields = ('id', 'name', 'description',)
+        fields = ('id', 'name', 'description', 'price',)
