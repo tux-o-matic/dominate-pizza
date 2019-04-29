0347fa4fa531130a2968b59da2de9f4cae12eb78
Benjamin Merot <4562092+tux-o-matic@users.noreply.github.com>
2019-04-29 13:00:49 +0200

    Undo last change

--- project/settings.py
+++ project/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-import django_opentracing
-import opentracing
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -72,7 +70,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_opentracing.OpenTracingMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
@@ -161,9 +158,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'wsgi/static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
-
-# Tracing
-
-OPENTRACING_TRACE_ALL = True
-OPENTRACING_TRACED_ATTRIBUTES = ['META']
-OPENTRACING_TRACING = django_opentracing.DjangoTracing()
