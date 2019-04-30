2957e95812f65a5338e03500279de8e0b20061df
Benjamin Merot <bem.its@dsg.dk>
2019-04-30 12:00:25 +0200

    Merged fixture. Template improvement. Basic listing of Piza

--- pizza/models.py
+++ pizza/models.py
@@ -10,50 +10,48 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.CharField(default=None, blank=True, max_length=255, null=True)
 
     price = models.DecimalField(default=0, max_digits=5, decimal_places=2)
     vat = models.DecimalField(default=0, max_digits=5, decimal_places=2)
-    currency = models.CharField(max_length=3)
+    currency = models.CharField(default='EUR', max_length=3)
+
+    organic = models.BooleanField(default=False)
+    vegetarian = models.BooleanField(default=False)
+    vegan = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
 
 
-class Food(Product):
-    organic = models.BooleanField(default=False)
-    vegetarian = models.BooleanField(default=False)
-    vegan = models.BooleanField(default=False)
-
-
-class Burger(Food):
+class Burger(Product):
     bacon = models.BooleanField(default=False)
     cheese = models.BooleanField(default=False)
     steaks = models.SmallIntegerField(default=0)
     others = models.CharField(max_length=255)
 
 
-class Meal(Food):
+class Meal(Product):
     pass
 
 
-class Topping(Food):
+class Topping(Product):
     pass
 
 
-class Pizza(Food):
+class Pizza(Product):
     calzone = models.BooleanField(default=False)
     diameter = models.SmallIntegerField(default=0)
 
     toppings = models.ManyToManyField(Topping)
 
 
-class Drink(Food):
+class Drink(Product):
     size = models.DecimalField(default=0, max_digits=3, decimal_places=2)
     alcohol = models.BooleanField(default=False)
 
 
-class Menu(Product):
+class Menu(models.Model):
     # inherited price is expected to be cheaper than sum of items in menu
     burgers = models.ManyToManyField(Burger, blank=True)
     drinks = models.ManyToManyField(Drink, blank=True)
